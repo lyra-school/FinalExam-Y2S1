1fbb8242a51665889b4ce0d161e7f1f17cca6411
lyra-school <s00233718@atu.ie>
2024-01-06 09:52:39 +0000

    Q1d

---
+++ FinalExam/BudgetItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam
+{
+    public enum BudgetItemType
+    {
+        Income,
+        Expense
+    }
+    public class BudgetItem
+    {
+        private string _name;
+        private decimal _amount;
+        private BudgetItemType _itemType;
+        private DateTime _date;
+        private bool _recurring;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public decimal Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public BudgetItemType ItemType
+        {
+            get { return _itemType; }
+            set { _itemType = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public bool Recurring
+        {
+            get { return _recurring; }
+            set { _recurring = value; }
+        }
+    }
+}
